4d8f6bb14d4cf3ee620785cbaff8267011c12f12
jungkyu kim <jgkim@realsn.com>
2023-07-12 14:11:18 +0900

    first commit

---
+++ src/component/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Button({ text }) {
+  return (
+    <button className="text-3xl font-bold bg-yellow-200 border-[10px] border-[#f00]">
+      {text}
+    </button>
+  );
+}
